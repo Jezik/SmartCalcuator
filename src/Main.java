1febfecdd49caa177bb3351cd89736f7457d1d5e
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-15 16:04:48 +0300

    Reordered while loop. The sum calculation extracted to a method and gets String[] array as a parameter

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    private static int sum(String[] array) {
+        int sum = 0;
+        for (String number : array) {
+            try {
+                sum += Integer.parseInt(number);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         boolean nextIteration = true;
 
         while (nextIteration) {
             String str = reader.readLine();
-            if (str.equals("/exit")) {
-                nextIteration = false;
-                System.out.println("Bye!");
+            if ("".equals(str)) {
                 continue;
             }
 
-            if (str.equals("")) {
-                continue;
+            if (str.equals("/exit")) {
+                nextIteration = false;
+                System.out.println("Bye!");
+            }
+            else if (str.equals("/help")) {
+                System.out.println("The program calculates the sum of numbers");
             }
             else {
                 String[] array = str.split(" ");
-                if (array.length == 1) {
-                    System.out.println(array[0]);
-                }
-                else {
-                    try {
-                        int a = Integer.parseInt(array[0]);
-                        int b = Integer.parseInt(array[1]);
-                        System.out.println(a + b);
-                    } catch (Exception e) {
-                        System.out.println(e.getMessage());
-                    }
-                }
+                System.out.println(sum(array));
             }
         }
     }
