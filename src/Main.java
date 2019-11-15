e7ef7b21634726e8173f4db8a6e3c127c6646516
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-15 15:02:51 +0300

    Loop for calculations. Ignore empty strings and repeat one-number strings

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        boolean nextIteration = true;
 
-        System.out.println(a + b);
+        while (nextIteration) {
+            String str = reader.readLine();
+            if (str.equals("/exit")) {
+                nextIteration = false;
+                System.out.println("Bye!");
+                continue;
+            }
+
+            if (str.equals("")) {
+                continue;
+            }
+            else {
+                String[] array = str.split(" ");
+                if (array.length == 1) {
+                    System.out.println(array[0]);
+                }
+                else {
+                    try {
+                        int a = Integer.parseInt(array[0]);
+                        int b = Integer.parseInt(array[1]);
+                        System.out.println(a + b);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+        }
     }
 }
