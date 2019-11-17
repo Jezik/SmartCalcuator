26a02449252ea0d4e8d43cc459d80e1b8f5d059a
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-17 11:47:35 +0300

    Now we can use one line expressions like X + Y - Z. Added subtraction. Even number of minuses gives a plus, and the odd number of minuses gives a minus

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Main {
 
-    private static int sum(String[] array) {
-        int sum = 0;
-        for (String number : array) {
-            try {
-                sum += Integer.parseInt(number);
+    private static int calculate(String[] array) {
+        int result = Integer.parseInt(array[0]);
+        for (int i = 1; i < array.length; ) {
+            if ("+".equals(array[i])) {
+                result += Integer.parseInt(array[i+1]);
+                i += 2;
             }
-            catch (Exception e) {
-                System.out.println(e.getMessage());
+            else if ("-".equals(array[i])) {
+                result -= Integer.parseInt(array[i+1]);
+                i += 2;
+            }
+            else {
+                System.out.println("DEBUG: Check this indexes, dummy");
+                break;
             }
         }
-        return sum;
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Pattern minusToMinusPtn = Pattern.compile("\\s-{11}\\s|\\s-{9}\\s|\\s-{7}\\s|\\s-{5}\\s|\\s-{3}\\s"); //The odd number of minuses gives a minus
+        Pattern minusToPlusPtn = Pattern.compile("\\s-{10}\\s|\\s-{8}\\s|\\s-{6}\\s|\\s-{4}\\s|\\s-{2}\\s"); //The even number of minuses gives a plus
+        Pattern plusToPlusPtn = Pattern.compile("\\+{2,}"); //Converts '++++++++' to a single '+'
+        Pattern spacesPtn = Pattern.compile("\\s{2,}"); //Removes extra spaces
         boolean nextIteration = true;
 
         while (nextIteration) {
@@ -32,11 +45,24 @@ public class Main {
                 System.out.println("Bye!");
             }
             else if (str.equals("/help")) {
-                System.out.println("The program calculates the sum of numbers");
+                System.out.println("The program calculates the result of an expression in a form \"X + Y - Z\"\n" +
+                        "The program supports both unary and binary minus operators.\n" +
+                "Consider that the even number of minuses gives a plus, and the odd number of minuses gives a minus! ");
             }
             else {
+                Matcher minusToMinusMhr = minusToMinusPtn.matcher(str);
+                str = minusToMinusMhr.replaceAll(" - ");
+                Matcher minusToPlusMhr = minusToPlusPtn.matcher(str);
+                str = minusToPlusMhr.replaceAll(" + ");
+                Matcher plusToPlusMhr = plusToPlusPtn.matcher(str);
+                str = plusToPlusMhr.replaceAll("+");
+                Matcher spacesMhr = spacesPtn.matcher(str);
+                str = spacesMhr.replaceAll(" ");
+
+                //System.out.println("DEBUG: " + str);
                 String[] array = str.split(" ");
-                System.out.println(sum(array));
+                //System.out.println("DEBUG: " + Arrays.toString(array));
+                System.out.println(calculate(array));
             }
         }
     }
