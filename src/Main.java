0d98e3e3c904f3232b72a57b4ead2f06d36a675a
Dmitry Baev <dmitry.baevtech@gmail.com>
2019-11-17 14:03:24 +0300

    Exception handling

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
 public class Main {
 
-    private static int calculate(String[] array) {
+    private static String prepareCalculationString(Pattern minusToMinus, Pattern minusToPlus, Pattern plusToPlus, Pattern spaces, String str) {
+        Matcher minusToMinusMhr = minusToMinus.matcher(str);
+        str = minusToMinusMhr.replaceAll(" - ");
+        Matcher minusToPlusMhr = minusToPlus.matcher(str);
+        str = minusToPlusMhr.replaceAll(" + ");
+        Matcher plusToPlusMhr = plusToPlus.matcher(str);
+        str = plusToPlusMhr.replaceAll("+");
+        Matcher spacesMhr = spaces.matcher(str);
+        str = spacesMhr.replaceAll(" ");
+        return str;
+    }
+
+    private static int calculate(String[] array) throws Exception {
         int result = Integer.parseInt(array[0]);
         for (int i = 1; i < array.length; ) {
             if ("+".equals(array[i])) {
@@ -19,8 +30,7 @@ public class Main {
                 i += 2;
             }
             else {
-                System.out.println("DEBUG: Check this indexes, dummy");
-                break;
+                throw new Exception();
             }
         }
         return result;
@@ -28,12 +38,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         Pattern minusToMinusPtn = Pattern.compile("\\s-{11}\\s|\\s-{9}\\s|\\s-{7}\\s|\\s-{5}\\s|\\s-{3}\\s"); //The odd number of minuses gives a minus
         Pattern minusToPlusPtn = Pattern.compile("\\s-{10}\\s|\\s-{8}\\s|\\s-{6}\\s|\\s-{4}\\s|\\s-{2}\\s"); //The even number of minuses gives a plus
         Pattern plusToPlusPtn = Pattern.compile("\\+{2,}"); //Converts '++++++++' to a single '+'
         Pattern spacesPtn = Pattern.compile("\\s{2,}"); //Removes extra spaces
-        boolean nextIteration = true;
 
+        boolean nextIteration = true;
         while (nextIteration) {
             String str = reader.readLine();
             if ("".equals(str)) {
@@ -49,20 +60,18 @@ public class Main {
                         "The program supports both unary and binary minus operators.\n" +
                 "Consider that the even number of minuses gives a plus, and the odd number of minuses gives a minus! ");
             }
+            else if (str.matches("/\\w*")) {
+                System.out.println("Unknown command");
+            }
             else {
-                Matcher minusToMinusMhr = minusToMinusPtn.matcher(str);
-                str = minusToMinusMhr.replaceAll(" - ");
-                Matcher minusToPlusMhr = minusToPlusPtn.matcher(str);
-                str = minusToPlusMhr.replaceAll(" + ");
-                Matcher plusToPlusMhr = plusToPlusPtn.matcher(str);
-                str = plusToPlusMhr.replaceAll("+");
-                Matcher spacesMhr = spacesPtn.matcher(str);
-                str = spacesMhr.replaceAll(" ");
-
-                //System.out.println("DEBUG: " + str);
+                str = prepareCalculationString(minusToMinusPtn, minusToPlusPtn, plusToPlusPtn, spacesPtn, str);
                 String[] array = str.split(" ");
-                //System.out.println("DEBUG: " + Arrays.toString(array));
-                System.out.println(calculate(array));
+                try {
+                    System.out.println(calculate(array));
+                }
+                catch(Exception e) {
+                    System.out.println("Invalid expression");
+                }
             }
         }
     }
